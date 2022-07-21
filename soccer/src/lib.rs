ed5f84fd743fff64621704971cd1074c65aebd51
Joonsun <juns98@naver.com>
2022-07-22 02:05:54 +0900

    Vector, Error handler

--- soccer/src/lib.rs
+++ soccer/src/lib.rs
@@ -1,9 +1,10 @@
 // crate는 항상 최상위
-mod players {
+mod player_module {
     // private is default
     pub struct Status {
         pub name: String,
         pub age: i8,
+        pub position: String,
     }
     pub struct Strength {
         pub kick: u32,
@@ -11,12 +12,22 @@ mod players {
         pub speed: u32,
         pub stamina: u32,
     }
+    pub struct Player {
+        pub status: Status,
+        pub strength: Strength, 
+    }
+    pub(crate) enum Position {
+        FW,
+        MF,
+        DF,
+        GK,
+    }
     impl Status {
-        pub fn junior (name: &str, age: i8) -> Status {
-            Status { name: String::from(name), age: i8::from(age) }
+        pub fn junior (name: &str, age: i8, position: &str) -> Status {
+            Status { name: String::from(name), age: i8::from(age), position: String::from(position) }
         }
-        pub fn senior (name: &str, age: i8) -> Status {
-            Status { name: String::from(name), age: i8::from(age) }
+        pub fn senior (name: &str, age: i8, position: &str) -> Status {
+            Status { name: String::from(name), age: i8::from(age), position: String::from(position) }
         }
     } 
     pub mod attacker {
@@ -37,27 +48,58 @@ mod players {
 }
 
 mod player;
+// use core::num;
+use std::io::stdin;
+
+use rand::Error;
 
 //bring path 
-use self::players:: {attacker, midfielder, defender };
-use self::players:: {Status, Strength};
+// use self::player_module:: {attacker, midfielder, defender };
+use self::player_module:: {Status, Strength, Position};
 use crate::player::init_player;
 
 
 pub fn play_game() {
     // Absolute path
-    init_player();
+    let mut players = Vec::new();
+    let num_players = 3;
+    for _i in 0..num_players {
+        let mut name = String::new();
+        let mut tempAge = String::new();
+        let mut position = String::new();
+        println!("Enter name: ");
+        let _len_name = stdin().read_line(&mut name);
+        println!("Enter age: ");
+        let _len_age = stdin().read_line(&mut tempAge);
+        let age = tempAge.trim().parse().expect("input not integer");
+        println!("Enter Position(only FW, MF, DF): ");
+        let _len_pos = stdin().read_line(&mut position);
+        
+        // 해결하기
 
-    crate::players::attacker::take_shot("Son");
-    // Relative path
-    players::attacker::take_shot("Son");
-    // use 
-    attacker::take_shot("Son");
-    midfielder::take_pass("kim");
-    defender::take_tackle("park");
+        // let check_position =  match position {
+        //     Err(Error) => panic!("Not proper position: {}", Error);
+        // }
 
-    let player_One_Status = Status::junior("kim", 13);
-    let playerTwoStatus = Status::senior("baek", 25);
+        players.push(init_player(&name, age, &position))
+    }
 
-    println!("{} {}",player_One_Status.name, player_One_Status.age);
+    println!("---Player List---");
+    for i in 0..num_players {
+        println!("Player Status   | name: {} age: {} position: {} ", &players[i].status.name, &players[i].status.age, &players[i].status.position);
+        println!("Player Strength | kick: {} pass: {} speed: {} stamina: {} ", &players[i].strength.kick, &players[i].strength.pass,&players[i].strength.speed,&players[i].strength.stamina);
+    }
+   
+    // crate::player_module::attacker::take_shot("Son");
+    // // Relative path
+    // player_module::attacker::take_shot("Son");
+    // // use 
+    // attacker::take_shot("Son");
+    // midfielder::take_pass("kim");
+    // defender::take_tackle("park");
+
+    // let player_One_Status = Status::junior("kim", 13);
+    // let playerTwoStatus = Status::senior("baek", 25);
+
+    // println!("{} {}",player_One_Status.name, player_One_Status.age);
 }
--- soccer/src/player.rs
+++ soccer/src/player.rs
@@ -1,17 +1,31 @@
 use rand::Rng;
-use crate::Strength;
+use crate::{Status, Strength, player_module::Player};
 
-pub fn init_player() {
+pub fn init_player(name: &str, age: i8, position: &str) -> Player {
     let kick = rand::thread_rng().gen_range(1..=100);
     let pass = rand::thread_rng().gen_range(1..=100);
     let speed = rand::thread_rng().gen_range(1..=100);
     let stamina = rand::thread_rng().gen_range(1..=100);
-
-    let temp_player = Strength {
+    
+    let player_strength = Strength {
         kick: kick,
         pass: pass,
         speed: speed,
         stamina: stamina,
     };
-    println!("Player info: kick {} pass {} speed {} stamina {}", temp_player.kick, temp_player.pass, temp_player.speed, temp_player.stamina);
+    let player_status = Status {
+        name: String::from(name),
+        age: age,
+        position: String::from(position),
+    };
+
+    // Player 초기화
+    let player = Player {
+        strength: player_strength,
+        status: player_status
+    };
+
+    return player;
+
+    // println!("Player info: kick {} pass {} speed {} stamina {}", temp_player.kick, temp_player.pass, temp_player.speed, temp_player.stamina);
 }
